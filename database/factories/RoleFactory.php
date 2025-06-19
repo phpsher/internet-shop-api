8bfe9fed44949a09cf4e109b220b442fa2b32ff3
Almaz Bogdanov <al.bogdanov@internet.ru>
2025-06-19 20:10:12 +0500

    Обновил миграции и фабрики

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -5,13 +5,14 @@ namespace Database\Factories;
 use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class RoleFactory extends Factory{
+class RoleFactory extends Factory
+{
     protected $model = Role::class;
 
     public function definition(): array
     {
         return [
-            //
+            'role' => $this->faker->randomElement(['user', 'admin'])
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Role::factory()->create(['role' => 'user']);
+        Role::factory()->create(['role' => 'admin']);
         Product::factory()->count(10)->create();
     }
 }
